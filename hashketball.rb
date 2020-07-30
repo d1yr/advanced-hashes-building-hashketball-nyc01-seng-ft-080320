7aff539f7ac913e6a2bdafecfe09e28d07ea8a6d
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-30 00:08:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
     :colors => ["Black","White"],
     :players => [
       
-      "Alan Anderson" => {
+      player_name: "Alan Anderson" => {
         :number => 0,
         :shoe => 16,
         :stats => {
@@ -16,7 +16,7 @@ def game_hash
         :blocks => 1,
         :slam_dunks => 1
         }},
-        "Reggie Evans" => {
+        player_name: "Reggie Evans" => {
         :number => 30,
         :shoe => 14,
         :points => 12,
@@ -26,7 +26,7 @@ def game_hash
         :blocks => 12,
         :slam_dunks => 7,
         },
-        "Brook Lopez" => {
+        player_name: "Brook Lopez" => {
         :number => 11,
         :shoe => 17,
         :points => 17,
@@ -36,7 +36,7 @@ def game_hash
         :blocks => 1,
         :slam_dunks => 15
         },
-        "Mason Plumlee" => {
+        player_name: "Mason Plumlee" => {
         :number => 1,
         :shoe => 19,
         :points => 26,
@@ -46,7 +46,7 @@ def game_hash
         :blocks => 8,
         :slam_dunks => 5
         },
-        "Jason Terry" => {
+        player_name: "Jason Terry" => {
         :number => 31,
         :shoe => 15,
         :points => 19,
