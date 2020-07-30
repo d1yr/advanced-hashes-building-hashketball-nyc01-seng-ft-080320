62ab096c82dbf45aabf1e1ea0492d64a999fa98c
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-30 00:12:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
     :colors => ["Black","White"],
     :players => [
       
-      {player_name: "Alan Anderson" => {
+      {player_name: "Alan Anderson",
         :number => 0,
         :shoe => 16,
         :points => 22,
@@ -14,8 +14,8 @@ def game_hash
         :steals => 3,
         :blocks => 1,
         :slam_dunks => 1
-        }},
-        {player_name: "Reggie Evans" => {
+        },
+        {player_name: "Reggie Evans",
         :number => 30,
         :shoe => 14,
         :points => 12,
@@ -24,8 +24,8 @@ def game_hash
         :steals => 12,
         :blocks => 12,
         :slam_dunks => 7,
-        }},
-        {player_name: "Brook Lopez" => {
+        },
+        {player_name: "Brook Lopez",
         :number => 11,
         :shoe => 17,
         :points => 17,
@@ -34,8 +34,8 @@ def game_hash
         :steals => 3,
         :blocks => 1,
         :slam_dunks => 15
-        }},
-        {player_name: "Mason Plumlee" => {
+        },
+        {player_name: "Mason Plumlee",
         :number => 1,
         :shoe => 19,
         :points => 26,
@@ -44,8 +44,8 @@ def game_hash
         :steals => 3,
         :blocks => 8,
         :slam_dunks => 5
-        }},
-       {player_name: "Jason Terry" => {
+        },
+        {player_name: "Jason Terry",
         :number => 31,
         :shoe => 15,
         :points => 19,
@@ -54,7 +54,7 @@ def game_hash
         :steals => 4,
         :blocks => 11,
         :slam_dunks => 1
-        }},
+        }
     ]
   }, :away => {
     :team_name => "Charlotte Hornets",
