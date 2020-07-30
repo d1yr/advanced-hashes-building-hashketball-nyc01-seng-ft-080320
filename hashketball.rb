1b8cc359e60ffe04740f41dfb6fe8fe5c9e9f8a8
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-30 00:09:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -8,7 +8,6 @@ def game_hash
       {player_name: "Alan Anderson" => {
         :number => 0,
         :shoe => 16,
-        :stats => {
         :points => 22,
         :rebounds => 12,
         :assists => 12,
