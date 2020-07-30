2d3d3e4c41eed740e6e31379ba9d219e72150370
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-30 00:02:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,27 @@
 # Write your code here!
+def game_hash
+  hasketball = {:home => {
+    :team_name => "Brooklyn Nets",
+    :colors => ["Black","White"],
+    :players => {
+      
+      :player_name => {
+        :number => 0,
+        :shoe => 16,
+        :stats => {
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1
+        }},
+    }
+  }, :away => {
+    :team_name => "Charlotte Hornets",
+    :colors => ["Turquoise","Purple"],
+    :players => {
+      
+    }
+  }}
+end
